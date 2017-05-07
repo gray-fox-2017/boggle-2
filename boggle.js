e1a44fdab996a8377fb7be8163cc43e66c2c1aaf
jamessraun <jeezybig@gmail.com>
2017-05-08 06:43:53 +0700

    update boggle2

--- boggle.js
+++ boggle.js
@@ -5,9 +5,10 @@ class Boggle{
     this._dictionary = //this._fs.readFileSync('data.js', 'utf8');
     ["ABA","ABAD","ABADI","ABAH","ABAI","ABAL","ABANG","ABANGAN","ABDI","ABDIKASI","ABDOMEN","ABDOMINAL","ABDUKSI","ABDUKTOR"];
     this._boggle= //this.boggle();
-    [['A','B','D'],
-    ['H','A','I'],
-    ['L','A','K']];
+    [['A','B','D','D'],
+    ['H','A','I','D'],
+    ['L','A','K','D'],
+    ['L','A','K','D']];
     this._dilewati=this.dilewati();
   }
 
@@ -53,9 +54,9 @@ class Boggle{
     if (this.kata(str))
     console.log(str);
 
-    for (let baris=i-1; baris<=i+1 && baris<3; baris++)
+    for (let baris=i-1; baris<=i+1 && baris<this._matriks; baris++)
     {
-      for (let kol=j-1; kol<=j+1 && kol<3; kol++){
+      for (let kol=j-1; kol<=j+1 && kol<this._matriks; kol++){
         if (baris>=0 && kol>=0 && !dilewati[baris][kol])
         this.cariKataPerIndex(boggle,dilewati, baris, kol, str);
       }
@@ -67,14 +68,14 @@ class Boggle{
   cariKata(boggle,dilewati){
     let i,j;
     let str = "";
-    for (i=0; i<3; i++)
-    for (j=0; j<3; j++)
+    for (i=0; i<this._matriks; i++)
+    for (j=0; j<this._matriks; j++)
     this.cariKataPerIndex(boggle, dilewati, i, j, str);
 
   }
 }
 
-let game = new Boggle(3);
+let game = new Boggle(4);
 console.log(game._boggle)
 console.log(game._dictionary);
 game.cariKata(game._boggle,game._dilewati);
